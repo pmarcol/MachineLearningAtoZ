579a414f374bb68e37a70610b7866b076aaa696f
Piotr Marcol <piotr.marcol@gmail.com>
2017-12-15 17:10:41 +0100

    Data preparation - final part

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing - e2e.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing - e2e.py
@@ -1,26 +1,46 @@
-# Data Preprocessing Template
-
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
 
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
+
+# Taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = "NaN", strategy = "mean", axis = 0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+
+# Encode categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+# Independent variable(s)
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+# Dependent variable
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-""" IN THE MOST OF THE TIMES WE WILL NOT NEED TO DO THE FEATURE SCALING, BECAUSE IT WILL BE A PART OF THE ALGORITHM.
+
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 
+"""
 # We do not need to do the feature scaling on the dependent variable, since it is categorical.
 # But if we had to, that is how it would be done:
 sc_y = StandardScaler()
--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.R
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.R
@@ -2,31 +2,19 @@
 
 # Importing the dataset
 dataset = read.csv('Data.csv')
-
-# Taking care of missing data
-dataset$Age = ifelse(is.na(dataset$Age),
-                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
-                     dataset$Age)
-dataset$Salary = ifelse(is.na(dataset$Salary),
-                     ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
-                     dataset$Salary)
-
-# Encoding categorical data
-dataset$Country = factor(dataset$Country,
-                         levels = c('France', 'Germany', 'Spain'),
-                         labels = c(1, 2, 3))
-dataset$Purchased = factor(dataset$Purchased,
-                           levels = c('Yes', 'No'),
-                           labels = c(1, 0))
+### JUST IN NEED WE WILL NEED TO TAKE A SUBSET OF THE IMPORTED DATASET.
+# dataset = [, 2:3]
 
 # Splitting the dataset into the Training set and Test set
 # install.packages('caTools')
 library(caTools)
 set.seed(123)
-split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
-# Feature Scaling
-# training_set = scale(training_set)
-# test_set = scale(test_set)
+###  IN THE MOST OF THE TIMES WE WILL NOT NEED TO DO THE FEATURE SCALING,
+### BECAUSE IT WILL BE A PART OF THE ALGORITHM.
+# # Feature Scaling
+# training_set[, 2:3] = scale(training_set[, 2:3])
+# test_set[, 2:3] = scale(test_set[, 2:3])
